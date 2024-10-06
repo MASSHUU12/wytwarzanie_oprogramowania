023bd65ce986b277253ff2c9e55760aa5ab9a56f
MASSHUU12 <gawrysiak.maciej@proton.me>
2024-10-06 13:41:24 +0200

    style(Pogodynka): reindent files using spaces instead of tabs & use LF line separator

--- Pogodynka/src/pl/edu/zut/wo/wzorce/pogodynka/wyświetl/WarunkiBieżąceWyświetlanie.java
+++ Pogodynka/src/pl/edu/zut/wo/wzorce/pogodynka/wyświetl/WarunkiBieżąceWyświetlanie.java
@@ -2,19 +2,19 @@ package pl.edu.zut.wo.wzorce.pogodynka.wyświetl;
 
 public class WarunkiBieżąceWyświetlanie {
 
-	private float temperatura;
-	private float wilgotność;
-	private float ciśnienie;
+    private float temperatura;
+    private float wilgotność;
+    private float ciśnienie;
 
-	public void aktualizacja(float temperatura, float wilgotność, float ciśnienie) {
-		this.temperatura = temperatura;
-		this.wilgotność = wilgotność;
-		this.ciśnienie = ciśnienie;
-		wyświetl();
-	}
-	
-	public void wyświetl() {
-		System.out.println("Warunki bieżące " + temperatura + " stopni C oraz " + wilgotność + "% wilgotność");
-	}
+    public void aktualizacja(float temperatura, float wilgotność, float ciśnienie) {
+        this.temperatura = temperatura;
+        this.wilgotność = wilgotność;
+        this.ciśnienie = ciśnienie;
+        wyświetl();
+    }
+
+    public void wyświetl() {
+        System.out.println("Warunki bieżące " + temperatura + " stopni C oraz " + wilgotność + "% wilgotność");
+    }
 
 }
